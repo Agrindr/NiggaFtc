5a535d6c88462f2418e0201c1f388b00475ba141
Agrindr <aginayargitirta@gmail.com>
2024-09-03 13:27:00 +0700

    Sigma

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/SimplifiedOpMode.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/SimplifiedOpMode.java
@@ -1,46 +1,31 @@
 package org.firstinspires.ftc.robotcontroller;
 
-import android.util.Size;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.hardware.DcMotor.RunMode;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-
-import java.util.List;
 
 @TeleOp
-public class NoAutonomous extends LinearOpMode {
+public class SimplifiedOpMode extends LinearOpMode {
 
     // Robot Control Variables
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotorEx rightDrive = null;
     private DcMotorEx leftDrive = null;
     private DcMotorEx lifter1 = null;
-    private CRServo StorageDoor1 = null;
-    private CRServo DownStorageDoor1 = null;
-    private CRServo DownStorageDoor2 = null;
+    private CRServo storageDoor1 = null;
+    private CRServo downStorageDoor1 = null;
+    private CRServo downStorageDoor2 = null;
 
     private Servo foodClawServo1 = null; // Servo for FoodClaw1
     private Servo foodClawServo2 = null; // Servo for FoodClaw2
     private DcMotorEx antena = null;    // New Core Hex Motor for Antena
-    private ColorSensor colorSensor = null; // Color Sensor for detecting orange
 
     private double speed = 0;
     private boolean isClawOpen = false; // Track if claws are open
-
-    // AprilTag Detection Variables
-    private VisionPortal visionPortal;
-    private AprilTagProcessor tagProcessor;
 
     // Servo position constants for food claws
     private static final double CLAW_OPEN_POSITION = 0.6;  // Example position to open claws
@@ -50,7 +35,6 @@ public class NoAutonomous extends LinearOpMode {
     public void runOpMode() {
         initRobotHardware();
         initFoodClaws();
-        initAprilTagDetection();
 
         telemetry.addData(">", "Touch Play to start OpMode");
         telemetry.update();
@@ -61,7 +45,6 @@ public class NoAutonomous extends LinearOpMode {
         while (opModeIsActive()) {
             controlRobot();
             controlFoodClaws(); // Add control for FoodClaws
-            detectAprilTags(); // Add AprilTag detection
 
             // Push telemetry to the Driver Station.
             telemetry.update();
@@ -75,9 +58,9 @@ public class NoAutonomous extends LinearOpMode {
         rightDrive = hardwareMap.get(DcMotorEx.class, "RightMotors");
         leftDrive = hardwareMap.get(DcMotorEx.class, "LeftMotors");
         lifter1 = hardwareMap.get(DcMotorEx.class, "LifterMotors1");
-        StorageDoor1 = hardwareMap.get(CRServo.class, "StorageDoor1");
-        DownStorageDoor1 = hardwareMap.get(CRServo.class, "DownStorageDoor1");
-        DownStorageDoor2 = hardwareMap.get(CRServo.class, "DownStorageDoor1");
+        storageDoor1 = hardwareMap.get(CRServo.class, "StorageDoor1");
+        downStorageDoor1 = hardwareMap.get(CRServo.class, "DownStorageDoor1");
+        downStorageDoor2 = hardwareMap.get(CRServo.class, "DownStorageDoor2");
 
         // Initialize the new Antena motor
         antena = hardwareMap.get(DcMotorEx.class, "Antena");
@@ -93,27 +76,10 @@ public class NoAutonomous extends LinearOpMode {
         // Initialize servos for food claws
         foodClawServo1 = hardwareMap.get(Servo.class, "FoodClawServo1");
         foodClawServo2 = hardwareMap.get(Servo.class, "FoodClawServo2");
-        colorSensor = hardwareMap.get(ColorSensor.class, "ColorSensor");
 
         // Set initial positions for the servos
         foodClawServo1.setPosition(CLAW_CLOSED_POSITION);
         foodClawServo2.setPosition(CLAW_CLOSED_POSITION);
-    }
-
-    private void initAprilTagDetection() {
-        // Initialize the camera and vision processor
-        tagProcessor = new AprilTagProcessor.Builder()
-                .setDrawAxes(true)
-                .setDrawTagID(true)
-                .setDrawCubeProjection(true)
-                .setDrawTagOutline(true)
-                .build();
-
-        visionPortal = new VisionPortal.Builder()
-                .addProcessor(tagProcessor)
-                .setCamera(hardwareMap.get(WebcamName.class, "Webcam1"))
-                .setCameraResolution(new Size(640, 480))
-                .build();
     }
 
     private void controlRobot() {
@@ -133,13 +99,16 @@ public class NoAutonomous extends LinearOpMode {
         } else {
             speed = 1;
         }
-        if (gamepad1.dpad_down){
-            DownStorageDoor1.setPower(-1);
-            DownStorageDoor2.setPower(-1);
-        }
-        if (gamepad1.dpad_down){
-            DownStorageDoor1.setPower(1);
-            DownStorageDoor2.setPower(1);
+
+        if (gamepad1.dpad_down) {
+            downStorageDoor1.setPower(-1);
+            downStorageDoor2.setPower(-1);
+        } else if (gamepad1.dpad_up) {
+            downStorageDoor1.setPower(1);
+            downStorageDoor2.setPower(1);
+        } else {
+            downStorageDoor1.setPower(0);
+            downStorageDoor2.setPower(0);
         }
 
         // Intake control
@@ -153,24 +122,17 @@ public class NoAutonomous extends LinearOpMode {
 
         // Storage door control
         if (gamepad1.a) {
-            StorageDoor1.setPower(1);
-
+            storageDoor1.setPower(1);
         } else if (gamepad1.b) {
-            StorageDoor1.setPower(-1);
-
+            storageDoor1.setPower(-1);
         } else {
-            StorageDoor1.setPower(0);
-
+            storageDoor1.setPower(0);
         }
 
         // Control the Antena motor
         if (gamepad2.right_bumper) {
             antena.setPower(1); // Activate Antena motor
-        } else {
-            antena.setPower(0); // Deactivate Antena motor
-        }
-
-        if (gamepad2.left_bumper) {
+        } else if (gamepad2.left_bumper) {
             antena.setPower(-1); // Activate Antena motor
         } else {
             antena.setPower(0); // Deactivate Antena motor
@@ -180,26 +142,19 @@ public class NoAutonomous extends LinearOpMode {
         rightDrive.setPower(RightPower * speed);
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "kanan (%.2f), kiri (%.2f)", RightPower, LeftPower);
+        telemetry.addData("Motors", "Right (%.2f), Left (%.2f)", RightPower, LeftPower);
         telemetry.addData("Antena", "Power: %.2f", antena.getPower());
     }
 
     private void controlFoodClaws() {
-        // Read color sensor values
-        int red = colorSensor.red();
-        int green = colorSensor.green();
-        int blue = colorSensor.blue();
-
-        boolean isOrange = (red > 200 && green > 100 && blue < 100);
-
-        if (isOrange) {
+        if (gamepad1.x) {
             if (!isClawOpen) {
                 // Open claws to a defined position
                 foodClawServo1.setPosition(CLAW_OPEN_POSITION);
                 foodClawServo2.setPosition(CLAW_OPEN_POSITION);
                 isClawOpen = true;
             }
-        } else {
+        } else if (gamepad1.b) {
             if (isClawOpen) {
                 // Close claws
                 foodClawServo1.setPosition(CLAW_CLOSED_POSITION);
@@ -208,31 +163,6 @@ public class NoAutonomous extends LinearOpMode {
             }
         }
 
-        telemetry.addData("Color Sensor", "Red: %d, Green: %d, Blue: %d", red, green, blue);
-    }
-
-    private void detectAprilTags() {
-        List<AprilTagDetection> detections = tagProcessor.getDetections();
-
-        if (!detections.isEmpty()) {
-            // Telemetry update for tag detected
-            telemetry.addLine("+-------------------------+");
-            telemetry.addLine("|   ____    _  __        |");
-            telemetry.addLine("|  / __ \\  | |/ /        |");
-            telemetry.addLine("| | |  | | | ' /         |");
-            telemetry.addLine("| | |  | | |  <          |");
-            telemetry.addLine("| | |__| | | . \\         |");
-            telemetry.addLine("|  \\____/  |_|\\_\\        |");
-            telemetry.addLine("+-------------------------+");
-
-            telemetry.addLine("Tag Detected:");
-            for (AprilTagDetection detection : detections) {
-                telemetry.addData("ID", detection.id);
-            }
-
-            gamepad1.setLedColor(0, 1, 0, 200); // Green LED for successful detection
-        } else {
-            gamepad1.setLedColor(1, 0, 0, 200); // Red LED if no tag detected
-        }
+        telemetry.addData("Claw Status", isClawOpen ? "Open" : "Closed");
     }
 }
